7eff7331fe6230f2187e3c231bb8c6070e4a36d7
Melusi Ntshangase <mentsha022@student.wethinkcode.co.za>
2023-07-31 12:05:10 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const value = document.getElementById("value");
+const counterBtns = document.querySelectorAll(".btn");
+
+let counter = 0;
+
+counterBtns.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    const target = e.currentTarget.classList;
+
+    if (target.contains("increase")) counter++;
+    if (target.contains("decrease")) counter--;
+    if (target.contains("reset")) counter = 0;
+
+    if (counter > 0) value.style.color = "green";
+    if (counter < 0) value.style.color = "red";
+    if (counter === 0) value.style.color = "black";
+
+    value.textContent = counter;
+  });
+});
